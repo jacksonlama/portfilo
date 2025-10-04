e2dfe61b1cb649fff8a5fa2eee52dcc5e1cb7603
TheSickness <imuttamkharel@gmail.com>
2025-10-04 10:00:28 +0545

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# 🌐 Personal Portfolio
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Portfolio 🚀</h1>
+      <p>Welcome to my React + Vite + TypeScript project!</p>
+    </div>
+  );
+}
+
+export default App;
